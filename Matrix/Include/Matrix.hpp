8d8542449d9fc0ab655188bda1486fc7dfd59a4d
Abhi Wargad <wargadabhi@gmail.com>
2026-02-27 16:17:29 +0530

    Matric class

--- Matrix/Include/Matrix.hpp
+++ Matrix/Include/Matrix.hpp
@@ -1,34 +1,3 @@
-// #ifndef MATRIX_HPP
-// #define MATRIX_HPP
-
-// #include <iostream>
-// using namespace std;
-
-// class Matrix {
-// protected:
-//     int rows, cols;
-//     double **data;
-
-// public:
-//     Matrix(int r, int c);
-//     Matrix(const Matrix &other);
-//     virtual ~Matrix();
-
-//     // Basic operations
-//     Matrix add(const Matrix &other);
-//     Matrix subtract(const Matrix &other);
-//     Matrix multiply(const Matrix &other);
-
-//     void setValue(int r, int c, double val);
-//     double getValue(int r, int c) const;
-
-//     void display() const;
-// };
-
-// #endif
-
-
-
 
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
@@ -41,9 +10,9 @@ protected:
     double **data;
 
 public:
-    Matrix(int r, int c);
-    Matrix(const Matrix &other);
-    virtual ~Matrix();
+    Matrix(int r, int c);    //Constructor
+    Matrix(const Matrix &other);  //copy constructor
+    virtual ~Matrix(); 
 
     Matrix add(const Matrix &other);
     Matrix subtract(const Matrix &other);
