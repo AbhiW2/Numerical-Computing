8ec52a25dc0a226b033a0e99f498798dfa9167e4
Abhi Wargad <wargadabhi@gmail.com>
2026-02-27 16:19:28 +0530

    main file

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -1,32 +1,101 @@
-#include "Matrix.hpp"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "../Include/Matrix.hpp"
+#include "../Include/GaussianElimination.hpp"
 
-int main() {
+using namespace std;
 
-    int r, c;
-    cout << "Enter rows and columns: ";
-    cin >> r >> c;
+int main()
+{
+    // Addition and Subtraction input files
+    ifstream matrixInput("input_matrix.txt");
+    ofstream matrixOutput("Add_sub_output.txt");
 
-    Matrix m1(r, c), m2(r, c);
+    if (matrixInput)
+    {
+        int r, c;
+        matrixInput >> r >> c;
 
-    cout << "\nMatrix 1\n";
-    m1.input();
+        Matrix A(r, c);
+        Matrix B(r, c);
 
-    cout << "\nMatrix 2\n";
-    m2.input();
+        // Read Matrix A
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+            {
+                double val;
+                matrixInput >> val;
+                A.setValue(i, j, val);
+            }
 
-    // Addition
-    Matrix sum = m1 + m2;
-    cout << "\nAddition of two matrices:";
-    sum.display();
+        // Read Matrix B
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+            {
+                double val;
+                matrixInput >> val;
+                B.setValue(i, j, val);
+            }
 
-    // Subtraction
-    Matrix diff = m1 - m2;
-    cout << "\nSubtraction of two matrices:";
-    diff.display();
+        Matrix C = A.add(B);
+        Matrix D = A.subtract(B);
 
-    // Gaussian Elimination on first matrix
-    cout << "\nPerforming Gaussian Elimination on Matrix 1";
-    m1.gaussianElimination();
+        matrixOutput << "Addition Result:\n";
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+                matrixOutput << C.getValue(i, j) << " ";
+            matrixOutput << endl;
+        }
+
+        matrixOutput << "\nSubtraction Result:\n";
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+                matrixOutput << D.getValue(i, j) << " ";
+            matrixOutput << endl;
+        }
+
+        cout << "Addition and Subtraction stored in Add_sub_output.txt\n";
+
+        matrixInput.close();
+        matrixOutput.close();
+    }
+    else
+    {
+        cout << "Matrix input file not found.\n";
+    }
+
+// Gaussian Elimination input files
+    ifstream Afile("49l.txt");   // coefficient matrix (49x49)
+    ifstream bfile("49r.txt");   // RHS vector (49x1)
+    ofstream gOutput("49_solution.txt");
+
+    if (!Afile || !bfile)
+    {
+        cout << "Error opening 49l.txt or 49r.txt\n";
+        return 1;
+    }
+
+    int n, m;
+    Afile >> n >> m;   // expected: 49 49
+
+    GaussianElimination G(n, m);
+
+    // Combine A and b into augmented matrix [A | b]
+    G.loadAugmentedMatrix(Afile, bfile);
+
+    vector<double> solution = G.solve();
+
+    for (int i = 0; i < n; i++)
+        gOutput << "x" << i + 1 << " = " << solution[i] << endl;
+
+    Afile.close();
+    bfile.close();
+    gOutput.close();
+
+    cout << "Gaussian solution stored in 49_solution.txt\n";
 
     return 0;
 }
