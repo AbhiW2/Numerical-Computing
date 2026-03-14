f378ed552988b753372708ab29f75c01205331ae
Abhi Wargad <wargadabhi@gmail.com>
2026-03-14 21:55:42 +0530

    Modify linearsystemsolver

--- Matrix/Include/LinearSystemSolver.hpp
+++ Matrix/Include/LinearSystemSolver.hpp
@@ -1,3 +1,29 @@
+// #ifndef LINEAR_SYSTEM_SOLVER_HPP
+// #define LINEAR_SYSTEM_SOLVER_HPP
+
+// #include "Matrix.hpp"
+// #include <vector>
+// #include <fstream>
+
+// class LinearSystemSolver : public Matrix {
+// public:
+//     LinearSystemSolver(int n);        // n equations → augmented matrix n×(n+1)
+//     LinearSystemSolver(int r, int c); // general constructor
+//     virtual ~LinearSystemSolver() = default;
+
+//     virtual void loadAugmentedMatrix(std::ifstream& Afile, std::ifstream& bfile) = 0;
+//     virtual std::vector<double> solve() = 0;
+// };
+
+// #endif // LINEAR_SYSTEM_SOLVER_HPP
+
+
+
+
+
+
+
+
 #ifndef LINEAR_SYSTEM_SOLVER_HPP
 #define LINEAR_SYSTEM_SOLVER_HPP
 
