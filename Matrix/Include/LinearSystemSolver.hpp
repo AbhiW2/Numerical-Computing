ac403f0bf881cb056bdeab63a5fa569521f8dd79
Abhi Wargad <wargadabhi@gmail.com>
2026-03-16 18:58:45 +0530

    Final linear header

--- Matrix/Include/LinearSystemSolver.hpp
+++ Matrix/Include/LinearSystemSolver.hpp
@@ -1,28 +1,3 @@
-// #ifndef LINEAR_SYSTEM_SOLVER_HPP
-// #define LINEAR_SYSTEM_SOLVER_HPP
-
-// #include "Matrix.hpp"
-// #include <vector>
-// #include <fstream>
-
-// class LinearSystemSolver : public Matrix {
-// public:
-//     LinearSystemSolver(int n);        // n equations → augmented matrix n×(n+1)
-//     LinearSystemSolver(int r, int c); // general constructor
-//     virtual ~LinearSystemSolver() = default;
-
-//     virtual void loadAugmentedMatrix(std::ifstream& Afile, std::ifstream& bfile) = 0;
-//     virtual std::vector<double> solve() = 0;
-// };
-
-// #endif // LINEAR_SYSTEM_SOLVER_HPP
-
-
-
-
-
-
-
 
 #ifndef LINEAR_SYSTEM_SOLVER_HPP
 #define LINEAR_SYSTEM_SOLVER_HPP
@@ -37,6 +12,7 @@ public:
     LinearSystemSolver(int r, int c); // general constructor
     virtual ~LinearSystemSolver() = default;
 
+    // Pure virtual functions - must be implemented by derived classes
     virtual void loadAugmentedMatrix(std::ifstream& Afile, std::ifstream& bfile) = 0;
     virtual std::vector<double> solve() = 0;
 };
