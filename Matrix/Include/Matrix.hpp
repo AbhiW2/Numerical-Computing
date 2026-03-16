15feac377cf0c096e2c0cd0bbd22f2b7eceeb7c5
Abhi Wargad <wargadabhi@gmail.com>
2026-03-16 18:57:39 +0530

    Final matrix header

--- Matrix/Include/Matrix.hpp
+++ Matrix/Include/Matrix.hpp
@@ -2,37 +2,53 @@
 #define MATRIX_HPP
 
 #include <iostream>
+#include <fstream>
 #include <stdexcept>
 
-class Matrix
-{
+class Matrix {
 protected:
     int rows;
     int cols;
-    double **data;
+    double** data;
 
 public:
-    // Constructor
-    Matrix(int r, int c);
-
-    // Copy Constructor (Deep Copy)
-    Matrix(const Matrix &other);
-
-    // Destructor
+    Matrix();
+    Matrix(int rows, int cols);
+    Matrix(const Matrix& m);
     virtual ~Matrix();
 
-    // Operator Overloading
-    Matrix operator+(const Matrix &other) const;
-    Matrix operator-(const Matrix &other) const;
-    Matrix operator*(const Matrix &other) const;
+    int getRows() const { return rows; }
+    int getCols() const { return cols; }
 
-    // Utility Functions
-    void setValue(int r, int c, double value);
-    double getValue(int r, int c) const;
-    void display() const;
+    void readFromFile(std::ifstream& fin);
+    void displayToFile(std::ofstream& fout) const;
 
-    int getRows() const;
-    int getCols() const;
+    Matrix operator+(const Matrix& m) const;
+    Matrix operator-(const Matrix& m) const;
+    Matrix operator*(const Matrix& m) const;
+
+    double&       operator()(int i, int j);
+    const double& operator()(int i, int j) const;
+
+    bool operator==(const Matrix& m) const;
+
+    friend std::istream& operator>>(std::istream& in, Matrix& m);
+    friend std::ostream& operator<<(std::ostream& out, const Matrix& m);
+
+    bool isSquare()             const;
+    bool isSymmetric()          const;
+    bool isIdentity()           const;
+    bool isNull()               const;
+    bool isDiagonal()           const;
+    bool isDiagonallyDominant() const;
+    bool isTranspose(const Matrix& m) const;
+
+    void   makeDiagonal();
+    void   makeDiagonallyDominant();
+    Matrix transpose() const;
+
+    double determinant() const;
+    Matrix inverse()     const;
 };
 
 #endif
