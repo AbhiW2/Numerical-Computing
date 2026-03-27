0c121b26f62ece973fcc15666dba68378425321f
Abhi Wargad <wargadabhi@gmail.com>
2026-03-27 11:17:23 +0530

    Header of Seidel

---
+++ Matrix/Include/GaussSeidel.hpp
@@ -0,0 +1,20 @@
+#ifndef GAUSS_SEIDEL_HPP
+#define GAUSS_SEIDEL_HPP
+
+#include "LinearSystemSolver.hpp"
+#include <vector>
+#include <cmath>
+
+class GaussSeidel : public LinearSystemSolver {
+private:
+    int maxIter;
+    double tol;
+
+public:
+    GaussSeidel(int n, int maxIter = 100, double tol = 1e-6);
+
+    void loadAugmentedMatrix(std::ifstream& Afile, std::ifstream& bfile) override;
+    std::vector<double> solve() override;
+};
+
+#endif
