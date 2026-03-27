52110b620215ee338e71fd04f24d5ae842295a8c
Abhi Wargad <wargadabhi@gmail.com>
2026-03-27 11:17:04 +0530

    Header of Jacobi

---
+++ Matrix/Include/GaussJacobi.hpp
@@ -0,0 +1,20 @@
+#ifndef GAUSS_JACOBI_HPP
+#define GAUSS_JACOBI_HPP
+
+#include "LinearSystemSolver.hpp"
+#include <vector>
+#include <cmath>
+
+class GaussJacobi : public LinearSystemSolver {
+private:
+    int maxIter;
+    double tol;
+
+public:
+    GaussJacobi(int n, int maxIter = 100, double tol = 1e-6);
+
+    void loadAugmentedMatrix(std::ifstream& Afile, std::ifstream& bfile) override;
+    std::vector<double> solve() override;
+};
+
+#endif
