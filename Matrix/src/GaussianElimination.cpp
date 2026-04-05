450e25ba643d04b3cfda1b4bbb75b0bf5a5d9f0b
Abhi Wargad <wargadabhi@gmail.com>
2026-04-05 21:28:00 +0530

    final code

--- Matrix/src/GaussianElimination.cpp
+++ Matrix/src/GaussianElimination.cpp
@@ -1,79 +1,64 @@
-
 #include "../Include/GaussianElimination.hpp"
 #include <cmath>
+#include <algorithm>
 #include <iostream>
+#include <iomanip>
+#include <stdexcept>
 
 using namespace std;
 
-static const double EPS = 1e-12;
+GaussianElimination::GaussianElimination(int n)
+    : LinearSystemSolver(n, n + 1)
+{}
 
-// Constructor
-GaussianElimination::GaussianElimination(int r, int c)
-    : Matrix(r, c) {}
-
-void GaussianElimination::loadAugmentedMatrix(ifstream &Afile, ifstream &bfile)
-{
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols - 1; j++)
-            Afile >> data[i][j];
-
-    for (int i = 0; i < rows; i++)
-        bfile >> data[i][cols - 1];
+void GaussianElimination::loadAugmentedMatrix(ifstream& Afile, ifstream& /*bfile*/) {
+    loadFromAugmentedFile(Afile);
 }
 
-// Forward elimination with pivot normalization
-void GaussianElimination::forwardElimination()
-{
-    for (int i = 0; i < rows; i++)
-    {
-        // partial pivoting
-        int maxRow = i;
-        for (int k = i + 1; k < rows; k++)
-            if (fabs(data[k][i]) > fabs(data[maxRow][i]))
-                maxRow = k;
+void GaussianElimination::forwardElimination() {
+    int n = rows;
+    for (int k = 0; k < n; k++) {
+        // Partial pivoting
+        int maxRow = k;
+        double maxVal = fabs((*this)(k, k));
+        for (int i = k + 1; i < n; i++) {
+            if (fabs((*this)(i, k)) > maxVal) {
+                maxVal = fabs((*this)(i, k));
+                maxRow = i;
+            }
+        }
+        if (maxRow != k)
+            for (int j = 0; j <= n; j++)
+                swap((*this)(k, j), (*this)(maxRow, j));
 
-        if (fabs(data[maxRow][i]) < EPS)
-            continue;
+        if (fabs((*this)(k, k)) < EPS) continue;
 
-        if (maxRow != i)
-            for (int j = 0; j < cols; j++)
-                swap(data[i][j], data[maxRow][j]);
-
-        // Normalize pivot row
-        double pivot = data[i][i];
-        for (int j = i; j < cols; j++)
-            data[i][j] /= pivot;
-
-        // Eliminate below
-        for (int k = i + 1; k < rows; k++)
-        {
-            double factor = data[k][i];
-            for (int j = i; j < cols; j++)
-                data[k][j] -= factor * data[i][j];
+        for (int i = k + 1; i < n; i++) {
+            double factor = (*this)(i, k) / (*this)(k, k);
+            for (int j = k; j <= n; j++)
+                (*this)(i, j) -= factor * (*this)(k, j);
         }
     }
 }
 
-// Back substitution (no division needed)
-vector<double> GaussianElimination::backSubstitution()
-{
-    vector<double> x(rows, 0.0);
-
-    for (int i = rows - 1; i >= 0; i--)
-    {
-        double sum = 0.0;
-        for (int j = i + 1; j < rows; j++)
-            sum += data[i][j] * x[j];
-
-        x[i] = data[i][cols - 1] - sum;
+vector<double> GaussianElimination::backSubstitution() {
+    int n = rows;
+    vector<double> x(n, 0.0);
+    for (int i = n - 1; i >= 0; i--) {
+        if (fabs((*this)(i, i)) < EPS) {
+            cout << "Warning: Free variable at row " << i << " (set to 0)\n";
+            continue;
+        }
+        double sum = (*this)(i, n);
+        for (int j = i + 1; j < n; j++)
+            sum -= (*this)(i, j) * x[j];
+        x[i] = sum / (*this)(i, i);
     }
-
     return x;
 }
 
-// Solve Ax = b
-vector<double> GaussianElimination::solve()
-{
+vector<double> GaussianElimination::solve() {
+    if (rows <= 0) throw runtime_error("Invalid matrix size");
     forwardElimination();
     return backSubstitution();
 }
